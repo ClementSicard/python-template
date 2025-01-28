4fbd8c782462615b5974081da5a51c244e497f88
Clément Sicard <clement.sicard@icloud.com>
2025-01-28 17:00:23 +0100

    lint

--- src/project_name/main.py
+++ src/project_name/main.py
@@ -4,9 +4,9 @@ from loguru import logger
 
 
 def main() -> None:
-  """Main entry point for the application script."""
-  logger.info("Call your main application code here")
+    """Main entry point for the application script."""
+    logger.info("Call your main application code here")
 
 
 if __name__ == "__main__":
-  main()
+    main()
--- src/project_name/main_test.py
+++ src/project_name/main_test.py
@@ -2,9 +2,9 @@
 
 
 def test_1() -> None:
-  """Test that the main function does not raise an AssertionError."""
-  from project_name.main import main
+    """Test that the main function does not raise an AssertionError."""
+    from project_name.main import main
 
-  main()
+    main()
 
-  assert True
+    assert True
